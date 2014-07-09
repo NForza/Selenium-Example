4fcdd2a6a275bf9fabcdae8fc4c7029880dc2bf1
Thomas Huijer <thomas.huijer@evision-software.com>
2014-07-09 10:53:58 +0200

    Added first simple test

---
+++ UnitTestProject1/ExampleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://www.nu.nl");
+                var element = driver.FindElement(By.Id("header"));
+                element.GetAttribute("class").Should().Contain("searchOptions");
+            }
+        }
+    }
+}
