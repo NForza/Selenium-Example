8a33727facbac608b32315e7c88dce4e1f66d4ce
Thomas Huijer <thomas.huijer@evision-software.com>
2014-07-09 12:51:15 +0200

    Added test application

--- SeleniumExample/ExampleTest.cs
+++ SeleniumExample/ExampleTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -10,13 +9,13 @@ namespace UnitTestProject1
     public class ExampleTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Root_Should_Have_3_H2_headers()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("http://www.nu.nl");
-                var element = driver.FindElement(By.Id("header"));
-                element.GetAttribute("class").Should().Contain("searchOptions");
+                driver.Navigate().GoToUrl("http://localhost:61226/");
+                var element = driver.FindElements(By.TagName("h2"));
+                element.Should().HaveCount(3);
             }
         }
     }
