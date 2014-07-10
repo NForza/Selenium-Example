3752206a151b23d47e12e9098cdc7805ea6297f1
Thomas Huijer <thomas@nforza.nl>
2014-07-10 23:53:34 +0200

    no message

---
+++ SeleniumExample/IWebDriverExtensions.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    public static class IWebDriverExtensions
+    {
+        public static IWebElement AboutMenuitem(this IWebDriver webDriver)
+        {
+            return webDriver.FindElement(By.LinkText("About"));
+        }
+    }
+}
