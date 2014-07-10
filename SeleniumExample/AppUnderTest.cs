527dfdaabaf7fcbc51920c93fe11da3b111a8f6e
Thomas Huijer <thomas@nforza.nl>
2014-07-10 23:20:39 +0200

    Added AppUndertest as first setup for a fluent api

---
+++ SeleniumExample/AppUnderTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    public class AppUnderTest
+    {
+        public static IWebDriver Start()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:61226/");
+            return driver;
+        }
+    }
+}
